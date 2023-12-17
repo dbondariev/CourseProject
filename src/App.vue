b6d8fb8732939838e44cf41bd506c096e6758c93
Dmytro Bondaryev <dima@Dmytros-MacBook-Pro.local>
2023-12-17 21:06:48 +0200

    created starting project structure as well as created one componenet that has props coming in

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,69 @@
 <template>
-  <h1>{{ greeting }}</h1>
+  <ul>
+    <learning-resource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :resource="resource"
+      :title="resource.title"
+      :link="resource.link"
+      :description="resource.description"
+    ></learning-resource>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+  components: {
+    LearningResource,
+  },
   data() {
     return {
-      greeting: 'Hello World!',
+      storedResources: [
+        {
+          id: 1,
+          title: 'Vue Documentation',
+          link: 'https://vuejs.org',
+          description: 'This is Vue documentation',
+        },
+        {
+          id: 2,
+          title: 'Google',
+          link: 'https://google.com',
+          description: 'This is google',
+        },
+        {
+          id: 3,
+          title: 'JavaScript',
+          link: 'https://javascript.info/',
+          description: 'Learn JavaScript',
+        },
+      ],
     };
   },
 };
 </script>
 
 <style>
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  margin-bottom: 1rem;
+}
 h1 {
-  color: red;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 2rem;
-  border: 1rem solid black;
-  background-color: darkgray;
+  margin-bottom: 0.5rem;
+}
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+p {
+  margin: 0;
 }
 </style>
---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link" target="_blank">View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'link', 'description'],
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  margin-bottom: 1rem;
+}
+h1 {
+  margin-bottom: 0.5rem;
+}
+</style>
