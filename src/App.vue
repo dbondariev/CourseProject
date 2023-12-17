29dbf20fb0bf914190b4763efde78207b5366a00
Dmytro Bondaryev <dima@Dmytros-MacBook-Pro.local>
2023-12-17 19:56:55 +0200

    created a project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ greeting }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hello World!',
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 2rem;
+  border: 1rem solid black;
+  background-color: darkgray;
+}
+</style>
